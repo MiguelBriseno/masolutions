9d892e4f68f6a2b832d6ed99a9d478c9cfad9f2e
mike <miguel.briseno.bustos@gmail.com>
2025-07-29 19:49:10 -0600

    Test

--- index.php
+++ index.php
@@ -2,11 +2,13 @@
 $title = "Home";
 require_once './components/general/header.php';
 require_once './components/general/navbar.php';
+$hero = ["Crea tu propio ", "software ", "y lleva la ", "productividad ", "a otro nivel."];
+$attended = "Atencion las 24 hrs";
+$button = "Agenda una sesion";
 ?>
 
 <main>
-    <h1>Bienvenido a mi sitio</h1>
-    <p>Este es el contenido principal de la página.</p>
+    
 </main>
 
 <?php
