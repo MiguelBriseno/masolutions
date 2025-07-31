2798b37ee016c8da75e35052dd504b7dc3d6af00
mike <miguel.briseno.bustos@gmail.com>
2025-07-31 14:29:55 -0600

    Done

--- components/index/hero.php
+++ components/index/hero.php
@@ -1,19 +1,18 @@
 <?php
 $hero = ["Crea tu propio ", "software ", "y lleva la ", "productividad ", "a otro nivel"];
-$attended = "Atención las 24 hrs";
 $button = "Agenda tu sesión";
 ?>
 
 <header class="header">
     <div class="header__left">
         <h1 class="left__title">
-            <?= $hero[0] ?> <span class="green"><?= $hero[1] ?></span> <?= $hero[2] ?> <span class="green"><?= $hero[3] ?></span> <?= $hero[4] ?><br>
-            <button class="left__button">
-                <i class="fa-solid fa-calendar"></i> <?= $button ?>
-            </button>
+            <?= $hero[0] ?><span class="green"><?= $hero[1] ?></span><?= $hero[2] ?><span class="green"><?= $hero[3] ?></span><?= $hero[4] ?>
         </h1>
+        <button class="left__button" onclick="window.open('https://calendly.com/d/cwty-bpz-z9f/reunion-puntual', '_blank')">
+            <i class="fa-solid fa-calendar"></i> <?= $button ?>
+        </button>
     </div>
     <div class="header__right">
-        <img src="./assets/display.webp" alt="Imagen de celular">
+        <img src="./assets/display.webp" alt="Imagen de celular mostrando software personalizado">
     </div>
 </header>
